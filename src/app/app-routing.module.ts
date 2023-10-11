92d06fe60ed6bfaab77f41bec1e5778567acf35d
Sulistya123 <sulistya.anggreini@mhs.politala.ac.id>
2023-10-11 13:33:11 +0800

    Nambah file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'grid',
     pathMatch: 'full'
   },
   {
@@ -18,6 +18,18 @@ const routes: Routes = [
   {
     path: 'profil',
     loadChildren: () => import('./profil/profil.module').then( m => m.ProfilPageModule)
+  },
+  {
+    path: 'toast',
+    loadChildren: () => import('./toast/toast.module').then( m => m.ToastPageModule)
+  },
+  {
+    path: 'grid',
+    loadChildren: () => import('./grid/grid.module').then( m => m.GridPageModule)
+  },
+  {
+    path: 'tugas',
+    loadChildren: () => import('./tugas/tugas.module').then( m => m.TugasPageModule)
   },
 ];
 
